b5d64cdf9c9183df83c2802347177f868860bf26
Michael COURTEILLE <m-courteille@logitud.fr>
2023-07-15 22:27:30 +0200

    update style, fix elements in table

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -2,20 +2,29 @@
 
 import React, { useState, useEffect } from 'react';
 
+interface Element {
+  symbol: string;
+  name: string;
+  atomicNumber: number;
+  atomicMass: string | number;
+  groupBlock: string;
+}
+
 const Homepage: React.FC = () => {
-  const [elements, setElements] = useState<
-    Array<{
-      symbol: string;
-      name: string;
-      atomicNumber: number;
-      atomicMass: string | number;
-    }>
-  >([]);
+  const [elements, setElements] = useState<Element[]>([]);
 
   useEffect(() => {
-    fetch('/api/elements/getAllElements')
-      .then(response => response.json())
-      .then(elements => setElements(elements.list));
+    const fetchElements = async () => {
+      try {
+        const response = await fetch('/api/elements/getAllElements');
+        const data = await response.json();
+        setElements(data.list);
+      } catch (error) {
+        console.error('Error fetching elements:', error);
+      }
+    };
+
+    fetchElements();
   }, []);
 
   const tableLayout: Array<Array<number | null>> = [
@@ -24,119 +33,131 @@ const Homepage: React.FC = () => {
     [11, 12, null, null, null, null, null, null, null, null, null, null, 13, 14, 15, 16, 17, 18],
     [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
     [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54],
-    [55, 56, 5771, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 63, 64, 65, 66],
+    [55, 56, 5771, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86],
     [87, 88, 89103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118],
   ];
 
+  const getClassForGroupBlock = (groupBlock: string): string => {
+    switch (groupBlock) {
+      case 'nonmetal':
+        return 'nonmetal';
+      case 'noble gas':
+        return 'noble-gas';
+      case 'alkali metal':
+        return 'alkali-metal';
+      case 'alkaline earth metal':
+        return 'alkaline-earth-metal';
+      case 'metalloid':
+        return 'metalloid';
+      case 'halogen':
+        return 'halogen';
+      case 'metal':
+        return 'metal';
+      case 'transition metal':
+        return 'transition-metal';
+      case 'lanthanoid':
+        return 'lanthanoid';
+      case 'actinoid':
+        return 'actinoid';
+      case 'post-transition metal':
+        return 'post-transition-metal';
+      default:
+        return '';
+    }
+  };
 
-  const generatePeriodicTable = () => {
-    const periodicTable: JSX.Element[] = [];
+  const generatePeriodicTable = (): JSX.Element[] => {
     let key = 0;
 
-    for (let column = 0; column < tableLayout[0].length; column++) {
-      const tableRow: JSX.Element[] = [];
+    const periodicTable = tableLayout[0].map((_, columnIndex) => (
+      <div key={columnIndex} className="tableColumn">
+        {tableLayout.map((row, rowIndex) => {
+          const elementNumber = row[columnIndex];
 
-      for (let row = 0; row < tableLayout.length; row++) {
-        const elementNumber = tableLayout[row][column];
-
-        if (elementNumber) {
-          const element = elements.find(elem => elem.atomicNumber === elementNumber);
-          if (element) {
-            tableRow.push(
-              <div key={key++} className={`element group${elementNumber}`}>
-                <div className="symbol">{element.symbol}</div>
-                <div className="name">{element.name}</div>
-                <div className="atomicNumber">{element.atomicNumber}</div>
-                <div className="atomicMass">{parseFloat(element.atomicMass).toFixed(2)}</div>
-              </div>
-            );
+          if (elementNumber) {
+            const element = elements.find((elem) => elem.atomicNumber === elementNumber);
+            if (element) {
+              const group = getClassForGroupBlock(element.groupBlock);
+              return (
+                <div key={key++} className={`element ${group}`}>
+                  <div className="symbol">{element.symbol}</div>
+                  <div className="name">{element.name}</div>
+                  <div className="atomicNumber">{element.atomicNumber}</div>
+                  <div className="atomicMass">
+                    {typeof element.atomicMass === 'number'
+                      ? element.atomicMass.toFixed(2)
+                      : parseFloat(element.atomicMass).toFixed(2)}
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <div key={key++} className={elementNumber === 5771 ? `element lanthanides` : elementNumber === 89103 ? `element actinides` : `element`}>
+                  <div className="name">
+                    <span className="rotate-text">
+                      {elementNumber === 5771 && 'lanthanides'}
+                      {elementNumber === 89103 && 'actinides'}
+                    </span>
+                  </div>
+                </div>
+              );
+            }
           } else {
-            tableRow.push(
-              <div key={key++} className={elementNumber === 5771 ? `element lanthanides` : elementNumber === 89103 ? `element actinides` : `element`}>
-                <div className="symbol">{elementNumber === 5771 ? "lanthanides" : "actinides"}</div>
+            return (
+              <div key={key++} className="element empty">
+                <div className="emptyText"></div>
               </div>
             );
           }
-        } else {
-          tableRow.push(
-            <div key={key++} className="element empty">
-              <div className="emptyText"></div>
-            </div>
-          );
-        }
-      }
-
-      periodicTable.push(
-        <div key={key++} className="tableRow">
-          {tableRow}
-        </div>
-      );
-    }
+        })}
+      </div>
+    ));
 
     return periodicTable;
   };
 
-  const generateLanthanides = () => {
-    const lanthanides: JSX.Element[] = [];
+  const generateElements = (startNumber: number, endNumber: number): JSX.Element[] => {
+    const elementsList: JSX.Element[] = [];
     let key = 0;
-  
-    for (let i = 0; i < 15; i++) {
-      const elementNumber = 57 + i;
-      const element = elements.find(elem => elem.atomicNumber === elementNumber);
-  
+
+    for (let i = startNumber; i <= endNumber; i++) {
+      const element = elements.find((elem) => elem.atomicNumber === i);
+
       if (element) {
-        lanthanides.push(
-          <div key={key++} className={`element lanthanides`}>
+        elementsList.push(
+          <div key={key++} className={`element ${startNumber === 57 ? 'lanthanides' : 'actinides'}`}>
             <div className="symbol">{element.symbol}</div>
             <div className="name">{element.name}</div>
             <div className="atomicNumber">{element.atomicNumber}</div>
-            <div className="atomicMass">{element.atomicMass}</div>
+            <div className="atomicMass">
+              {typeof element.atomicMass === 'number'
+                ? element.atomicMass.toFixed(2)
+                : parseFloat(element.atomicMass).toFixed(2)}
+            </div>
           </div>
         );
       } else {
-        lanthanides.push(
+        elementsList.push(
           <div key={key++} className="element empty">
             <div className="emptyText">-</div>
           </div>
         );
       }
     }
-  
-    return lanthanides;
+
+    return elementsList;
   };
-  
-  const generateActinides = () => {
-    const actinides: JSX.Element[] = [];
-    let key = 0;
-  
-    for (let i = 0; i < 15; i++) {
-      const elementNumber = 89 + i;
-      const element = elements.find(elem => elem.atomicNumber === elementNumber);
-  
-      if (element) {
-        actinides.push(
-          <div key={key++} className={`element actinides`}>
-            <div className="symbol">{element.symbol}</div>
-            <div className="name">{element.name}</div>
-            <div className="atomicNumber">{element.atomicNumber}</div>
-            <div className="atomicMass">{element.atomicMass}</div>
-          </div>
-        );
-      } else {
-        actinides.push(
-          <div key={key++} className="element empty">
-            <div className="emptyText">-</div>
-          </div>
-        );
-      }
-    }
-  
-    return actinides;
+
+  const generateLanthanides = (): JSX.Element[] => {
+    return generateElements(57, 71);
   };
-  
+
+  const generateActinides = (): JSX.Element[] => {
+    return generateElements(89, 103);
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Tableau périodique des éléments</h1>
       <div className="periodicTable">{generatePeriodicTable()}</div>
       <h2>Lanthanides</h2>
